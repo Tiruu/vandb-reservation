49a0b02ccda750659eb70198ad6ecada1c9cb8e9
Tiruu <lino.thebault@gmail.com>
2025-04-13 10:35:55 +0200

    Centralized header

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion des Réservations | Gestion Tireuses</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="styles/main.css">
-    <link rel="icon" type="image/png" href="medias/logo-vandb-noir-plein.png" />
-</head>
-<body>
-    <?php include('nav.php'); ?>
+<?php include('nav.php'); ?>
 
     <div class="container-fluid mt-4">
         <h2 class="mb-4">Gestion des Réservations</h2>
--- nav.php
+++ nav.php
@@ -1,4 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendrier | Gestion Tireuses</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <img src="medias/logo-vandb-noir-plein.png" alt="" style="width: 5rem;">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
