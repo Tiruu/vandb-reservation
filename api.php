199424c192a6ad85f24060f490563d547f9a6cf0
Tiruu <97034382+Tiruu@users.noreply.github.com>
2026-09-07 13:01:05 +0200

    api changes

--- api.php
+++ api.php
@@ -4,21 +4,6 @@ ini_set('display_errors',   '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// CORS preflight handling
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type');
-    exit(0);
-}
-
-// now your normal headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=utf-8");
-
-
-
-
 // Connecting to the DB
 $host = "localhost";
 $user = "root";
